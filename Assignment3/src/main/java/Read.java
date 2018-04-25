4629f45648f93e861237073b91eca69b53378050
Mohamad Adam Rustam <36665321+adamrustam@users.noreply.github.com>
2018-04-25 22:31:02 +0800

    Update Read.java

--- Assignment3/src/main/java/Read.java
+++ Assignment3/src/main/java/Read.java
@@ -1,18 +1,18 @@
 
-import java.io.BufferedWriter ;
-import java.io.File ;
-import java.io.FileInputStream ;
-import java.io.FileNotFoundException ;
-import java.io.FileWriter ;
-import java.io.IOException ;
-import java.io.Writer ;
-import java.util.Iterator ;
-import org.apache.poi.ss.usermodel.Cell ; 
-import org.apache.poi.ss.usermodel.DataFormatter ;
-import org.apache.poi.ss.usermodel.Row ;
-import org.apache.poi.ss.usermodel.Sheet ;
-import org.apache.poi.ss.usermodel.Workbook ;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook ;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.Cell; 
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -25,68 +25,68 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook ;
  * @author adamrustam
  */
 public class Read {
-    private static final String FILE_NAME = "/Users/adamrustam/Desktop/sample.xlsx" ;
+    private static final String FILE_NAME = "/Users/adamrustam/Desktop/sample.xlsx";
     public void baca() {
         
-        Writer writer = null ;
-        boolean line = true ;
+        Writer writer = null;
+        boolean line = true;
         
         try {
             
-            DataFormatter df = new DataFormatter() ;
+            DataFormatter df = new DataFormatter();
 
-            FileInputStream excelFile = new FileInputStream(new File(FILE_NAME)) ;
-            Workbook workbook = new XSSFWorkbook(excelFile) ;
-            Sheet datatypeSheet = workbook.getSheetAt(0) ;
-            Iterator<Row> iterator = datatypeSheet.iterator() ;
+            FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
+            Workbook workbook = new XSSFWorkbook(excelFile);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = datatypeSheet.iterator();
             
-            File file = new File("/Users/adamrustam/240803-STIW3054-A172-A1.wiki/Home.md") ;
-            writer = new BufferedWriter(new FileWriter(file)) ;
+            File file = new File("/Users/adamrustam/240803-STIW3054-A172-A1.wiki/Home.md");
+            writer = new BufferedWriter(new FileWriter(file));
       
           
 
             while (iterator.hasNext()) {
 
-                Row currentRow = iterator.next() ;
-                Iterator<Cell> cellIterator = currentRow.iterator() ;
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
 
                 while (cellIterator.hasNext()) {
 
-                    Cell currentCell = cellIterator.next() ;
-                    String val = df.formatCellValue(currentCell) ;
+                    Cell currentCell = cellIterator.next();
+                    String val = df.formatCellValue(currentCell);
                     
-                    //System.out.print(val +" ") ;
+                    //System.out.print(val +" ");
                   
                     
-                    writer.write(val +" ") ; 
+                    writer.write(val +" "); 
                     
                     
                   
 
                 }
-                //System.out.println() ;
-                writer.write("\n") ;
+                //System.out.println();
+                writer.write("\n");
                 
                 
 
             }
         } catch (FileNotFoundException e) {
-            e.printStackTrace() ;
+            e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace() ;
+            e.printStackTrace();
         }
         
         try {                 
             if (writer != null) {                     
-                writer.close() ;                 
+                writer.close();                 
             }             
         } catch (IOException e) {                 
-            e.printStackTrace() ;             
+            e.printStackTrace();             
         }   
         
     }
     
     public void See() {
-        System.out.println("Assignment 2") ;
+        System.out.println("Assignment 2");
     }
 }
