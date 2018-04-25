849c840d4d84cba78bcff50f82e5e78e989a71bc
Mohamad Adam Rustam <36665321+adamrustam@users.noreply.github.com>
2018-04-25 22:14:31 +0800

    Update Calculate.java

--- Assignment3/src/main/java/Calculate.java
+++ Assignment3/src/main/java/Calculate.java
@@ -1,15 +1,15 @@
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader ;
+import java.io.BufferedWriter ;
+import java.io.File ;
+import java.io.FileNotFoundException ;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.LineNumberReader;
-import java.io.StringReader;
-import java.io.Writer;
+import java.io.FileReader ;
+import java.io.FileWriter ;
+import java.io.IOException ;
+import java.io.LineNumberReader ;
+import java.io.StringReader ;
+import java.io.Writer ;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -24,47 +24,47 @@ import java.io.Writer;
 public class Calculate {
     
 
-    Writer writer = null;
+    Writer writer = null ;
        
     
     public void kira() throws Exception {
     
-    System.out.println ("Counting Words");       
-       FileReader fr = new FileReader ("/Users/adamrustam/240803-STIW3054-A172-A1.wiki/Home.md");        
-       BufferedReader br = new BufferedReader (fr);     
-       String line = br.readLine ();
-       int count = 0;
-       int bilLine = 0;
+    System.out.println ("Counting Words") ;       
+       FileReader fr = new FileReader ("/Users/adamrustam/240803-STIW3054-A172-A1.wiki/Home.md") ;        
+       BufferedReader br = new BufferedReader (fr) ;     
+       String line = br.readLine () ;
+       int count = 0 ;
+       int bilLine = 0 ;
        while (line != null) {
-          String []parts = line.split(" ");
-          bilLine++;
+          String []parts = line.split(" ") ;
+          bilLine++ ;
           for( String w : parts)
           {
-            count++;        
+            count++ ;        
           }
-          line = br.readLine();
+          line = br.readLine() ;
        }
        
 
        
-       File file = new File("/Users/adamrustam/240803-STIW3054-A172-A2.wiki/Home2.md");
-       writer = new BufferedWriter(new FileWriter(file));
+       File file = new File("/Users/adamrustam/240803-STIW3054-A172-A2.wiki/Home2.md") ;
+       writer = new BufferedWriter(new FileWriter(file)) ;
        
-       System.out.println("Words : "+count);
-       writer.write("* Words : "+count +"\n");
+       System.out.println("Words : "+count) ;
+       writer.write("* Words : "+count +"\n") ;
        
-       System.out.println("Lines : "+bilLine);
-       writer.write("* Lines : "+bilLine +"\n");
+       System.out.println("Lines : "+bilLine) ;
+       writer.write("* Lines : "+bilLine +"\n") ;
        
-       System.out.println("Average words in one line : "+count / bilLine + " Words ");
-       writer.write("* Average words in one line : "+count / bilLine + " Words "+"\n");
+       System.out.println("Average words in one line : "+count / bilLine + " Words ") ;
+       writer.write("* Average words in one line : "+count / bilLine + " Words "+"\n") ;
        
        try {                 
             if (writer != null) {                     
-                writer.close();                 
+                writer.close() ;                 
             }             
         } catch (IOException e) {                 
-            e.printStackTrace();             
+            e.printStackTrace() ;             
         }
     
     
