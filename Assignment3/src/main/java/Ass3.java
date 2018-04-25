d96e8d73f4df2de3a613911fec2626364c0346aa
Mohamad Adam Rustam <36665321+adamrustam@users.noreply.github.com>
2018-04-25 22:25:56 +0800

    Update Ass3.java

--- Assignment3/src/main/java/Ass3.java
+++ Assignment3/src/main/java/Ass3.java
@@ -11,25 +11,25 @@ public class Ass3 {
 
     public static void main(String[] args) throws Exception {
         
-       final long start = System.currentTimeMillis()/1000 ;
-       //System.out.println("start:"+start) ;
-       Read ABC = new Read() ;
-       ABC.See() ;
-       ABC.baca() ;
+       final long start = System.currentTimeMillis()/1000;
+       //System.out.println("start:"+start);
+       Read ABC = new Read();
+       ABC.See();
+       ABC.baca();
        
-       Calculate CDE = new Calculate() ;
-       CDE.kira() ;
+       Calculate CDE = new Calculate();
+       CDE.kira();
        
        
        
     
-       final long end = System.currentTimeMillis()/1000 ;
-       //System.out.println(end) ;
-       long elapsedTime = end - start ;
-       //System.out.println("ElapsedTime : "+elapsedTime) ;
+       final long end = System.currentTimeMillis()/1000;
+       //System.out.println(end);
+       long elapsedTime = end - start;
+       //System.out.println("ElapsedTime : "+elapsedTime);
        
-       //long timeSeconds = TimeUnit.MILLISECONDS.toSeconds(elapsedTime) ;
-       System.out.println("Tooks : "+elapsedTime+"s") ;
+       //long timeSeconds = TimeUnit.MILLISECONDS.toSeconds(elapsedTime);
+       System.out.println("Tooks : "+elapsedTime+"s");
        
        
        
